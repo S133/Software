5d8052444541e50c19cea022ac1e18df572d0e84
施扬恩 <43131095+S133@users.noreply.github.com>
2019-03-20 12:02:45 +0800

    出题量 计算值

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using 四则运算2._0;
 
 namespace ConsoleApp1
 {
@@ -10,30 +12,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            
+            DataTable dt = new DataTable();
             Random number = new Random(); //实例化一个随机数           
-            string[] op = new string[] { "＋", "－", "×", "÷" };//定义一个符号数组
-            for (int i = 0; i < 10; i++)
+            string[] operatos = new string[] { "＋", "－", "×", "÷" };
+            string[] operatos1 = new string[] { "+", "-", "*", "/" };
+            Console.WriteLine("要生成多少题，最多支持10000题");
+            int draw = Convert.ToInt32(Console.ReadLine()); //获取出题量
+            Console.WriteLine("要生多少以内的计算题(0~100之内)");
+            int difficulty = Convert.ToInt32(Console.ReadLine()); //获取出题量                                                                 
+            for (int s = 0; s < draw; s++)
             {
-                double number1 = number.Next(1, 10);//随机一个10以内的数
-                double number2 = number.Next(1, 10);//随机一个10以内的数
-                int opnext = number.Next(0, 4);//随机一个 符号
-                double s = 0;
-                switch (opnext)
-                {
-                    case 0:
-                        s = number1 + number2;
-                        break;
-                    case 1:
-                        s = number1 - number2;
-                        break;
-                    case 2:
-                        s = number1 * number2;
-                        break;
-                    case 3:
-                        s = number1 / number2;
-                        break;
-                }
-                Console.WriteLine(number1 + op[opnext] + number2 + "=" + s);//输出公式
+
+                int nubere_shu = number.Next(1, 4);
+                Console.WriteLine(Operation.formula(dt, number, difficulty, operatos, operatos1, nubere_shu));
             }
             Console.ReadKey();
         }
